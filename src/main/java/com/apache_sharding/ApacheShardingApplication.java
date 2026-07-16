506357ed2aa946804b737b50629699f85e5b69a7
manivannancsmca <manivannancsmca@gmail.com>
2026-07-16 08:06:47 +0530

    first commit

---
+++ src/main/java/com/apache_sharding/ApacheShardingApplication.java
@@ -0,0 +1,13 @@
+package com.apache_sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheShardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apache_sharding/ApacheShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache_sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheShardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
